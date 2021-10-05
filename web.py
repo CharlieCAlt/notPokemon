0d572a5e95f2dae7e64652759567d8a58361ab10
dan220 <danielstorer54@gmail.com>
2021-10-05 14:23:55 +0100

    Added some templates to website

--- web.py
+++ web.py
@@ -1,16 +1,20 @@
 from flask import Flask, render_template, request
+from create_tables import CreateTables
 
 app = Flask(__name__)
 
+database = CreateTables()
+
 @app.route("/")
 def pokedex():
-    return render_template('pokedex.html')
+    return render_template('pokedex2.html')
 
 '''
 
 @app.route("/displayPokemons")
 def display_pokemons():
     # get pokemons from database
+    getPokemon()
     pokemons = get_pokemons(book_title)
     pokemon_list = pokemons.values.tolist()
     return render_template('info.html', pokemons=pokemon_list)
