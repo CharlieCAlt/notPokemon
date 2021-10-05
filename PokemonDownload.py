ed08c677bf570fe07c0cb2af1575f3d58ff57cbb
CharlieCAlt <charlie.crane@altran.com>
2021-10-05 12:29:20 +0100

    Alex said do it

--- PokemonDownload.py
+++ PokemonDownload.py
@@ -1,19 +1,62 @@
 import requests
 import SQL_Data
 
-# pokemon url https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151
+class Pokemon:
+    """ A Pokemon with Name, Artwork, Attack, Defense and Type stored for database input"""
+
+    def __init__(self, name, artwork, attack, defense, types):
+        self.__name=name
+        self.__artwork = artwork
+        self.__attack = attack
+        self.__defense = defense
+        self.__types = types
 
 def getPokemon():
     r = requests.get(f'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151')
     response = r.json()
     results = response['results']
+    pokenumber = 1
     for pokemon in results:
-        print (pokemon['name'])
-        pokeName = pokemon['name']
-        SQL_Data.addPokemon(pokeName)
+        print('------------')
+        print(pokemon['name'])
+        Name = pokemon['name']
+        if 0 < pokenumber < 152:
+            stats = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokenumber}/")
+            response = stats.json()
+            results = response['stats']
+            pictures = response['sprites']
+            Artwork = pictures['front_default']
+            type_data = response['types']
+            breed = type_data[0]
+            type = breed['type']
+            Type1 = type['name']
+            try:
+                new_breed = type_data[1]
+                new_type = new_breed['type']
+                Type2 = new_type['name']
+                print(Type1, Type2)
+                Types = (Type1, Type2)
+            except IndexError:
+                print(Type1)
+                Types = Type1
+            print(Artwork)
+            for data in results:
+                figure = data['stat']
+                if figure['name'] == 'attack':
+                    print(figure['name'], data['base_stat'])
+                    Attack = (data['base_stat'])
+                if figure['name'] == 'defense':
+                    print(figure['name'], data['base_stat'])
+                    Defense = data['base_stat']
+                else:
+                   pass
+            bob = Pokemon(Name, Artwork, Attack, Defense, Types)
+            input_data = (pokenumber, Name, Artwork, Attack, Defense, str(Types))
+            print(input_data)
+            pokenumber += 1
+        else:
+            pass
+        SQL_Data.addPokemon(input_data)
 
-
-def getPokeInfo():
-    pass
-
-getPokemon()
+if __name__ == '__main__':
+    getPokemon()
--- SQL_Data.py
+++ SQL_Data.py
@@ -2,17 +2,24 @@ import sqlite3
 from sqlite3 import Error
 
 try:
-    conn = sqlite3.connect('Pokedex.db', check_same_thread=False)
+    conn = sqlite3.connect('pokedex.db', check_same_thread=False)
 except Error as e:
     print(e)
 cursor = conn.cursor()
 
-def addPokemon(pokeName):
+def addPokemon(input_data):
     addition = f"""
-    INSERT INTO pokemon (Name)
+    INSERT INTO Pokedex (ID, Name, Artwork, Attack, Defense, Types)
     VALUES(
+    ?,
+    ?,
+    ?,
+    ?,
+    ?,
     ?
     )
     """
-    cursor.execute(addition, pokeName)
+    cursor.execute(addition, input_data)
     conn.commit()
+
+# This is a change
