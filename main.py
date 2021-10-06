e6c0e6451bd4b0cd290355e26dd3fe3e96a98c0e
CharlieCAlt <charlie.crane@altran.com>
2021-10-06 12:06:16 +0100

    Deck Testing

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from create_tables import Database
 import pokemon_download
+from sqlite3 import IntegrityError
+from create_deck import Deck
 
 def main():
     database = Database()
     database.CreateTables()
-    pokemon_download.getPokemon(database)
+    try:
+        pokemon_download.getPokemon(database)
+    except IntegrityError:
+        pass
+    deck = Deck()
+    answer = deck.shuffle()
+    deck_a, deck_b = answer
 
 if __name__ == '__main__':
     main()
