032ba52d6a1badaee45ea6fff2132abeb74073e5
dan220 <danielstorer54@gmail.com>
2021-10-06 14:41:14 +0100

    Cleaned up code

--- web.py
+++ web.py
@@ -11,24 +11,23 @@ button_count = 0
 
 @app.route("/")
 def pokedex():
-    return render_template('pokedex2.html')
+    return render_template('pokedex.html')
 
 
 @app.route("/downloadPokemons")
 def download_pokemons():
     global button_count
-    print(button_count)
     if button_count % 2 == 0:
         database.delete_table()
         database.createTables()
         pokemon_download.getPokemon(database)
-        alert = 'Do you want to override pokemon data?'
+        alert = 'Do you want to override pokemon data? If yes, click Download Pokemons again'
     else:
         alert = None
     button_count += 1
     names = database.returnNames()
     names_list = names.values.tolist()
-    return render_template('pokedex2.html', names=names_list, alert=alert)
+    return render_template('pokedex.html', names=names_list, alert=alert)
 
 @app.route("/showPokemons")
 def show_pokemons():
@@ -36,10 +35,8 @@ def show_pokemons():
     names_list = names.values.tolist()
     name = request.args.get('name')
     data = database.getAll(name)
-    print(data)
     data_list = data.values.tolist()
-    print(data_list)
-    return render_template('pokedex2.html', names=names_list, name=data_list[0][1], art=data_list[0][2],
+    return render_template('pokedex.html', names=names_list, name=data_list[0][1], art=data_list[0][2],
                            attack=data_list[0][3], defense=data_list[0][4], type=data_list[0][5])
 
 
