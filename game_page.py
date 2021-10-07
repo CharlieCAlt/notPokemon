ca00f9753176659e7f5af6db9c16a7cd957ab431
CharlieCAlt <charlie.crane@altran.com>
2021-10-07 11:37:54 +0100

    first card in deck is now shown on game page

--- game_page.py
+++ game_page.py
@@ -1,6 +1,16 @@
-#game page scripts and functions
 from flask import Flask, render_template, redirect
 from create_deck import Deck
+import pokemon_download
+import sqlite3
+from sqlite3 import Error
+
+try:
+    conn = sqlite3.connect('pokedex.db', check_same_thread=False)
+except Error as e:
+    print(e)
+    raise e
+cursor = conn.cursor()
+
 
 class GamMan:
 
@@ -16,5 +26,26 @@ class GamMan:
         else:
             return render_template('game_template.html')
 
+#
+# deck = Deck()
+# deck_a, deck_b = deck.shuffle()
+#
+# print(deck_a)
 
-    
+def pokemon_data(deck_a):
+    pokemon_find = deck_a[0]
+    pokemon_find = str(pokemon_find)
+    print(type(pokemon_find))
+    find_pokemon = """
+        SELECT Name, Attack, Defense, Types
+        FROM Pokedex
+        WHERE ID=?
+                """
+    cursor.execute(find_pokemon, (pokemon_find,))
+    data = cursor.fetchone()
+    return data
+
+# print(pokemon_data())
+# answer=pokemon_data()
+# name, attack, defense, types = answer
+# print(name)
--- web_menu.py
+++ web_menu.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, redirect
 from create_deck import Deck
+import game_page
 
 app = Flask(__name__)
 
@@ -11,10 +12,13 @@ def index():
 def game():
     deck=Deck()
     deck_a, deck_b = deck.shuffle()
+    values = game_page.pokemon_data(deck_a)
+    name, attack, defense, types = values
+    print(values)
     if len(deck_a) < 1:
         return redirect("/")
     else:
-        return render_template('game_template.html')
+        return render_template('game_template.html', deck_a = deck_a, deck_b=deck_b, name=name, attack=attack, defense=defense, types=types)
 
 @app.route("/test")
 def test():
