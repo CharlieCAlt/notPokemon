dd39cf177dbf750777e24b91680f44459230fc2a
R3ganator <alexander.regan@hotmail.com>
2021-10-11 16:02:51 +0100

    Added two separate html files that relate to the split deck, and changed a lot of code to include ajax

--- web_menu.py
+++ web_menu.py
@@ -11,47 +11,64 @@ app = Flask(__name__)
 
 class Button:
     def __init__(self):
-        self.check = False
         self.deck_a = []
         self.deck_b = []
-        self.counter = -1
+        self.counterA = 70
+        self.counterB = 70
+        deck = Deck()
+        self.deck_a, self.deck_b = deck.shuffle()
+
 
 var = Button()
+
 
 @app.route("/")
 def index():
     return render_template('index.html')
 
+
 @app.route("/game")
 def game():
-    global var
-    while var.check is False:
-        deck = Deck()
-        var.deck_a, var.deck_b = deck.shuffle()
-        #print(var.deck_a, var.deck_b)
-        var.check = True
-    if len(var.deck_a) < 1:
-        return redirect("/")
-    else:
-        var.counter += 1
-        value = next(var.deck_a, var.deck_b, var.counter)
-        values, values2, counter = value
-        name, attack, defense, types = values
-        name2, attack2, defense2, types2 = values2
-        return render_template('game_template.html', deck_a=var.deck_a, deck_b=var.deck_b, name=name, attack=attack, defense=defense, types=types, name2=name2, attack2=attack2, defense2=defense2, types2=types2, counter=counter)
+    return render_template('game_template.html')
 
-def next(deck_a, deck_b, counter):
+
+@app.route("/cardStatsA")
+def cardA():
+    global var
+    remaining_a = len(var.deck_a) - var.counterA
+    if remaining_a == 0:
+        var.counterA = 0
+        remaining_a = len(var.deck_a) - var.counterA
     database = Database()
-    values = database.pokemonData(deck_a, counter)
-    values2 = database.pokemonData(deck_b, counter)
-    return values, values2, counter
+    values = database.pokemonData(var.deck_a, var.counterA)
+    name, attack, defense, types = values
+    var.counterA += 1
+    return render_template('cardStatsA.html', deck_a=var.deck_a, name=name, attack=attack, defense=defense, types=types, counter1=var.counterA-1, remaining1=remaining_a)
+
+
+@app.route("/cardStatsB")
+def cardB():
+    global var
+    remaining_b = len(var.deck_b) - var.counterB
+    if remaining_b == 0:
+        var.counterB = 0
+        remaining_b = len(var.deck_b) - var.counterB
+    database = Database()
+    values2 = database.pokemonData(var.deck_b, var.counterB)
+    name2, attack2, defense2, types2 = values2
+    var.counterB += 1
+    return render_template('cardStatsB.html', deck_b=var.deck_b, name2=name2, attack2=attack2, defense2=defense2, types2=types2, counter2=var.counterB-1, remaining2=remaining_b)
+
 
 @app.route("/test")
 def test():
     return render_template('test.html')
+
+
 @app.route("/pokedex")
 def display_pokedex():
     return render_template('pokedex.html')
+
 
 @app.route("/downloadPokemons")
 def download_pokemons():
@@ -68,6 +85,7 @@ def download_pokemons():
     names_list = names.values.tolist()
     return render_template('pokedex.html', names=names_list, alert=alert)
 
+
 @app.route("/showPokemons")
 def show_pokemons():
     names = database.returnNames()
@@ -77,5 +95,6 @@ def show_pokemons():
     data_list = data.values.tolist()
     return render_template('card.html', names=names_list, name=data_list[0][1], art=data_list[0][2],
                            attack=data_list[0][3], defense=data_list[0][4], type=data_list[0][5])
+
 
 if __name__ == "__main__": app.run()
