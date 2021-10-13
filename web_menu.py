d961049c8bb72977005105ce043272560e54af8e
R3ganator <alexander.regan@hotmail.com>
2021-10-13 12:19:51 +0100

    changed all wrong variables after importing player class, and added a try except loop for deleting tables

--- web_menu.py
+++ web_menu.py
@@ -30,57 +30,57 @@ def game():
 def startA():
     global var
     database = Database()
-    values = database.pokemonData(var.player_1.deck, var.counterA)
+    values = database.pokemonData(var.player_1.deck, var.player_1.counter)
     name, attack, defense, type1, type2 = values
-    remaining_a = len(var.player_1.deck) - var.counterA
+    remaining_a = len(var.player_1.deck) - var.player_1.counter
     if remaining_a == 0:
-        var.counterA = 0
-        remaining_a = len(var.player_1.deck) - var.counterA
+        var.player_1.counter = 0
+        remaining_a = len(var.player_1.deck) - var.player_1.counter
     return render_template('cardStatsA.html', deck_a=var.player_1.deck, name=name, attack=attack, defense=defense, type1=type1,
-                           type2=type2, counter1=var.counterA, remaining1=remaining_a)
+                           type2=type2, counter1=var.player_1.counter, remaining1=remaining_a)
 
 @app.route("/startB")
 def startB():
     global var
     database = Database()
-    values = database.pokemonData(var.player_2.deck, var.counterB)
+    values = database.pokemonData(var.player_2.deck, var.player_2.counter)
     name, attack, defense, type1, type2 = values
-    remaining_b = len(var.player_2.deck) - var.counterB
+    remaining_b = len(var.player_2.deck) - var.player_2.counter
     if remaining_b == 0:
-        var.counterB = 0
-        remaining_b = len(var.player_2.deck) - var.counterB
+        var.player_2.counter = 0
+        remaining_b = len(var.player_2.deck) - var.player_2.counter
     return render_template('cardStatsB.html', deck_b=var.player_2.deck, name2=name, attack2=attack, defense2=defense,
-                           typeB1=type1, typeB2=type2, counter2=var.counterB, remaining2=remaining_b)
+                           typeB1=type1, typeB2=type2, counter2=var.player_2.counter, remaining2=remaining_b)
 
 
 @app.route("/cardStatsA")
 def cardA():
     global var
-    var.counterA += 1
-    remaining_a = len(var.player_1.deck) - var.counterA
+    var.player_1.counter += 1
+    remaining_a = len(var.player_1.deck) - var.player_1.counter
     if remaining_a == 0:
-        var.counterA = 0
-        remaining_a = len(var.player_1.deck) - var.counterA
+        var.player_1.counter = 0
+        remaining_a = len(var.player_1.deck) - var.player_1.counter
     database = Database()
-    values = database.pokemonData(var.player_1.deck, var.counterA)
+    values = database.pokemonData(var.player_1.deck, var.player_1.counter)
     name, attack, defense, type1, type2 = values
     return render_template('cardStatsA.html', deck_a=var.player_1.deck, name=name, attack=attack, defense=defense, type1=type1,
-                           type2=type2, counter1=var.counterA, remaining1=remaining_a)
+                           type2=type2, counter1=var.player_1.counter, remaining1=remaining_a)
 
 
 @app.route("/cardStatsB")
 def cardB():
     global var
-    var.counterB += 1
-    remaining_b = len(var.player_2.deck) - var.counterB
+    var.player_2.counter += 1
+    remaining_b = len(var.player_2.deck) - var.player_2.counter
     if remaining_b == 0:
-        var.counterB = 0
-        remaining_b = len(var.player_2.deck) - var.counterB
+        var.player_2.counter = 0
+        remaining_b = len(var.player_2.deck) - var.player_2.counter
     database = Database()
-    values = database.pokemonData(var.player_2.deck, var.counterB)
+    values = database.pokemonData(var.player_2.deck, var.player_2.counter)
     name, attack, defense, type1, type2 = values
     return render_template('cardStatsB.html', deck_b=var.player_2.deck, name2=name, attack2=attack, defense2=defense,
-                           typeB1=type1, typeB2=type2, counter2=var.counterB, remaining2=remaining_b)
+                           typeB1=type1, typeB2=type2, counter2=var.player_2.counter, remaining2=remaining_b)
 
 
 @app.route("/test")
